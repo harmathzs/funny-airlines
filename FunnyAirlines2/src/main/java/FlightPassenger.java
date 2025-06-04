19692758ab847d0f4315cbc98aead0683ff7c73e
Zsolt Harmath <harmathzs@kkando.hu>
2025-06-04 09:08:35 +0200

    Solve task 1

--- FunnyAirlines2/src/main/java/FlightPassenger.java
+++ FunnyAirlines2/src/main/java/FlightPassenger.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FlightPassenger {
+public class FlightPassenger {
+    // FlightNumber	Source	Destination	PassengerName	PassengerId	TicketID	SeatNumber	Price
+    public String FlightNumber;
+    public String Source;
+    public String Destination;
+    public String PassengerName;
+    public int PassengerId;
+    public String TicketID;
+    public String SeatNumber;
+    public int Price;
+
+    public FlightPassenger(String fileLine) {
+        String[] split = fileLine.split(";");
+        this.FlightNumber = split[0];
+        this.Source = split[1];
+        this.Destination = split[2];
+        this.PassengerName = split[3];
+        this.PassengerId = Integer.parseInt(split[4]);
+        this.TicketID = split[5];
+        this.SeatNumber = split[6];
+        this.Price = Integer.parseInt(split[7]);
+    }
 }
--- FunnyAirlines2/src/main/java/Main.java
+++ FunnyAirlines2/src/main/java/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static List<FlightPassenger> flightPassengerList = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // 1. feladat
+        flightPassengerList = new ArrayList<>();
+        File fin = new File("funny-airlines.csv");
+        Scanner scanner = new Scanner(fin);
+        String firstLine = scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String fileLine = scanner.nextLine();
+            flightPassengerList.add(new FlightPassenger(fileLine));
+        }
+        System.out.println("1. Adatok beolvasva.");
+    }
 }
